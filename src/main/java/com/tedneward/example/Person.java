9900f21b534ad60424b23b01a45e2c38f9f1a80c
Yulong Tan <yulongtan67@gmail.com>
2017-01-05 23:57:21 -0800

    add setters, getters, and compares

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -3,11 +3,10 @@ package com.tedneward.example;
 import java.beans.*;
 import java.util.*;
 
-public class Person {
-  private int age;
-  private String name;
+public class Person implements Comparable<Person> {
+  private int age, count = 0;
+  private String name, ssn;
   private double salary;
-  private String ssn;
   private boolean propertyChangeFired = false;
 
   public Person() {
@@ -18,6 +17,66 @@ public class Person {
     this.name = n;
     this.age = a;
     this.salary = s;
+    this.count++;
+  }
+
+  class AgeComparator implements Comparable <Person> {
+    private Person p1, p2;
+
+    public AgeComparator(Person p1, Person p2) {
+      this.p1 = new Person(p1.name, p1.age, p1.salary);
+      this.p2 = new Person(p2.name, p2.age, p2.salary);
+    }
+
+    public int compareTo(Person other) {
+      return Integer.compare(p1.age, p2.age);
+    }
+  }
+
+  public int compareTo(Person other) {
+    return Double.compare(other.salary, this.salary);
+  }
+
+  public int count() {
+    return this.count;
+  }
+
+  public boolean equals(Person other) {
+    return this.name.equals(other.name) && this.age == other.age;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    if (age < 0) {
+      throw new IllegalArgumentException();
+    }
+    this.age = age;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    if (name.equals("")) {
+      throw new IllegalArgumentException();
+    }
+    this.name = name;
+  }
+
+  public double getSalary() {
+    return this.salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+  public String getSSN() {
+    return this.ssn;
   }
 
   public void setSSN(String value) {
